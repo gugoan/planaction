2452cdbe4b455d677c05b0a2831301c8c7641b0b
Gustavo G. Andrade <gugoan@hotmail.com>
2018-04-29 09:19:16 -0300

    minify css

--- assets/LoginAsset.php
+++ assets/LoginAsset.php
@@ -12,8 +12,8 @@ class LoginAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        'css/font-awesome.min.css',
+        'css/ionicons.min.css',
         'css/AdminLTE.min.css',
         'plugins/iCheck/square/blue.css',
     ];
